95ae0798156b8b5ff4e0563a0ed54abfd94ec0e5
Reid Williams <rwilliams@ideo.com>
2016-12-20 15:45:46 -0800

    initial sketch of local-state methods

---
+++ src/local-state/index.js
@@ -0,0 +1,33 @@
+/* Local state operations include tracking which nodes a node is subscribed
+to, which messages (for each subscription) have been delivered to user code,
+and any user code local state that needs to persist accross process failures. */
+
+
+// SUBSCRIPTIONS
+
+// get existing subscriptions
+// remove subscription from cache
+// get head hash for existing subscription
+// set head hash for existing subscription
+// clear head hash for existing subscription
+
+// LOCAL STATE
+// For a later rev?
+// get arbitrary data
+// put arbitrary data
+
+// FAULT TOLERANT LIFECYCLE
+// startBatch: marks the beginning of a series of operations that should be atomic
+// endBatch: marks the end of a series of operations that should be atomic
+// A batch should be persisted atomically. This may only be relevant in Node.js not browser
+
+module.exports = {
+	getSubscriptions,
+	removeSubscription,
+	getHeadForSubscription,
+	setHeadForSubscription,
+	clearHeadForSubscription
+}
+
+
+
