c2d37f260d2adc75daee163aa74e2772b4c79601
Gavin McDermott <gavinmcdermott@gmail.com>
2017-01-20 15:58:03 -0800

    wip

--- test/node/add.js
+++ test/node/add.js
@@ -1,8 +1,12 @@
 const io = require('socket.io-client')
 
+
 const promisify = require('es6-promisify')
 const expect = require('chai').expect
 const WebRTCStar = require('libp2p-webrtc-star')
+
+const PeerId = require('peer-id')
+const PeerInfo = require('peer-info')
 
 
 const cmd = require('./../utils/cmd-runner')
@@ -38,10 +42,10 @@ const sioOptions = {
 
 // 188.166.203.82:20000
 
-const conn = io.connect('http://10.2.2.162:10000', sioOptions)
-conn.on('connect', () => {
-  console.log('connected!!')
-})
+// const conn = io.connect('http://10.2.2.162:10000', sioOptions)
+// conn.on('connect', () => {
+//   console.log('connected!!')
+// })
 
 let rtc
 
@@ -52,24 +56,32 @@ nodeFactory.create()
   })
   .then(() => {
     const id = node.identity.id
-    node._ipfs._peerInfo.multiaddr.add(`/libp2p-webrtc-star/ip4/10.2.2.162/tcp/10000/ws/ipfs/${id}`)
+    // node._ipfs._peerInfo.multiaddr.add(`/libp2p-webrtc-star/ip4/10.2.2.162/tcp/10000/ws/ipfs/${id}`)
 
     let addP = promisify(node._ipfs._libp2pNode.swarm.transport.add)
     rtc = new WebRTCStar()
-    rtc.createListener((data) => {
-      console.log('hey: ', data)
-    })
+    // rtc.createListener((data) => {
+    //   console.log('hey: ', data)
+    // })
     return addP('wstar', rtc)
   })
   // .then(() => {
-  //   const dialP = promisify(rtc.dial)
-  //   return dialP(`/libp2p-webrtc-star/ip4/10.2.2.162/tcp/10000/ws/ipfs/`)
+  //   console.log('trying to dial...')
+  //   // const dialP = promisify(rtc.dial)
+
+  //   const id = PeerId.createFromB58String('QmQy9MiS5weQfpYP48cnBPoRgLbfK2zmpCQAU7tNb9VbAJ')
+  //   const info = new PeerInfo(id)
+
+  //   console.log(info)
+
+  //   return node._ipfs._libp2pNode.swarm.dial(info)
+  //   // return node._ipfs._libp2pNode.swarm.dial(`/libp2p-webrtc-star/ip4/10.2.2.162/tcp/10000/ws/ipfs/QmQy9MiS5weQfpYP48cnBPoRgLbfK2zmpCQAU7tNb9VbAJ`)
   // })
-  // .then(() => {
-  //   return node._ipfs.files.cat(contentHash)
-  // })
+  // // .then(() => {
+  // //   return node._ipfs.files.cat(contentHash)
+  // // })
   // .then((data) => {
-  //   console.log('RECEIVED: ', data)
+  //   console.log('dialed: ', data)
   // })
   .catch((err) => console.log('ERR: ', err))
 
